bfa0beff653c5987d9552415e475d41298291d34
Lei Zhou <zhoulei091@126.com>
2017-08-11 22:40:23 +0800

    Update pub.py

--- pub.py
+++ pub.py
@@ -1,70 +1,55 @@
 #!/usr/bin/python
-# -*-coding:utf-8-*-
+#author zhoulei
+#
+#程序主入口(可将数据传入redis通道、可传入Java端做处理等)
+#
+#-*-coding:utf-8-*-
 import redis
-from get_addr import get_addr
 from usbinfo import usbinfo
 from conv_usbinfo import lon_lat_Conv
 import datetime
 import time
 import ConfigParser
-import MySQLdb
-from baidu_map_api import gen_bd_address
+from getGpsJsonStr import  getGpsOfJsonStr
+from gpsClient import *
 
 cf = ConfigParser.ConfigParser()
 cf.read("gps.conf")
-frequency = float(cf.get("frequency", "myfrequency"))
-channel = cf.get("channel", "mychannel")
+frequency=float(cf.get("frequency", "myfrequency"))
+channel=cf.get("channel", "mychannel")
+default_lat=cf.get("default_lon_lat","lat")
+gis_mode=cf.get("GIS_MODE","gis_mode")
+gis_frequency=int(cf.get("GIS_FREQUENCY","gis_frequency"))
 
-default_lat = cf.get("default_lon_lat", "lat")
-
-gis_mode = cf.get("GIS_MODE", "gis_mode")
-gis_frequency = int(cf.get("GIS_FREQUENCY", "gis_frequency"))
-
-car_id = cf.get("CAR_ID", "car_id")
-car = car_id.decode('gbk').encode('utf-8')
-
-host = cf.get("MYSQL_INFO", "host")
-port = int(cf.get("MYSQL_INFO", "port"))
-user = cf.get("MYSQL_INFO", "user")
-passwd = cf.get("MYSQL_INFO", "passwd")
-db = cf.get("MYSQL_INFO", "db")
+car_id=cf.get("CAR_ID","car_id")
+car=car_id.decode('gbk').encode('utf-8')
 
 rc = redis.Redis(host='127.0.0.1')
 ##ps = rc.pubsub()
 
-for num in range(1, 360000):
-    cur_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+for num in range(1,360000):
+    print '-------------------------------------------------------------------' , num
+    gislist =lon_lat_Conv()
+    if gislist is not None :
+        mylist = list()
+        mylist.append(gislist[0])
+        mylist.append(gislist[1])
+        print "redis  list ===",mylist
+        print "gis list===",gislist
+        cur_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        mydict={"latitude":float(mylist[0]),"current_time":str(cur_time),"longitude":float(mylist[1])}
+        print "Publish To Redis channel :"+str(mydict.get('latitude'))+','+str(mydict.get('longitude'))+','+mydict.get('current_time')
+        if num%gis_frequency==0 and gis_mode=='on' and str(gislist[0])!=default_lat:
+            print 'Publish To Netty Server**************************'
+            msg = getGpsOfJsonStr(gislist,car_id)
+            if msg is not None:
+                try :
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    client = doConnect(s)
+                    revice = sendData(client, msg)
+                    doClose(client)
+                except Exception, e:
+                    print e
 
-    mylist = lon_lat_Conv(usbinfo)
-    ##    mystr=str(mylist[1])+','+str(mylist[0])+','+str(cur_time)
-    mydict = {"latitude": float(mylist[0]), "current_time": str(cur_time), "longitude": float(mylist[1])}
-    ##    print("Publish To Redis gps_data_ch :"+mydict)
-
-    ##    mydict={"current_time":cur_time,"current_address":cur_addr}
-    print "Publish To Redis channel :" + str(mydict.get('latitude')) + ',' + str(
-        mydict.get('longitude')) + ',' + mydict.get('current_time')
-
-    ##    print gen_bd_address(mylist[0],mylist[1])
-    ##    print mydict
-    ##    print type(mydict)
-
-    ##    if num%gis_frequency==0 and gis_mode=='on':
-    if num % gis_frequency == 0 and gis_mode == 'on' and str(mylist[0]) != default_lat:
-        conn = MySQLdb.connect(
-            host=host,
-            port=port,
-            user=user,
-            passwd=passwd,
-            db=db,
-            charset='utf8'
-        )
-        cur = conn.cursor()
-        print 'insert into mysql!!!!!!!!!!!!!!!!!!!!!!!!!!'
-        sqli = "insert into base_info(car_id,lon,lat,time) values(%s,%s,%s,%s)"
-        cur.execute(sqli, (car, str(mydict.get('longitude')), str(mydict.get('latitude')), cur_time))
-        cur.close()
-        conn.commit()
-        conn.close()
-
-    rc.publish(channel, mydict)
+        rc.publish(channel, mydict)
     time.sleep(frequency)
